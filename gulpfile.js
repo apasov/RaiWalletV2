0dc4482682679d066eb262ea5767a6d5216b8f20
Jame <jaimehgb@gmail.com>
2018-01-12 02:15:46 +0100

    Updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,5 +17,16 @@ elixir(function(mix) {
        'resources/assets/css/creative.css',
        'resources/assets/css/sidebar.css',
        '../../../node_modules/jquery-toast-plugin/dist/jquery.toast.min.css'
-    ], 'public/css/app.css');
+    ], 'public/css/app.css')
+    .copy('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.eot', 'public/fonts/fontawesome-webfont.eot')
+    .copy('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.svg', 'public/fonts/fontawesome-webfont.svg')
+    .copy('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.ttf', 'public/fonts/fontawesome-webfont.ttf')
+    .copy('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.woff', 'public/fonts/fontawesome-webfont.woff')
+    .copy('vendor/fortawesome/font-awesome/fonts/fontawesome-webfont.woff2', 'public/fonts/fontawesome-webfont.woff2')
+    .copy('vendor/fortawesome/font-awesome/fonts/FontAwesome.otf', 'public/fonts/FontAwesome.otf')
+    .copy('vendor/twbs/bootstrap/fonts/glyphicons-halflings-regular.eot', 'public/fonts/glyphicons-halflings-regular.eot')
+    .copy('vendor/twbs/bootstrap/fonts/glyphicons-halflings-regular.svg', 'public/fonts/glyphicons-halflings-regular.svg')
+    .copy('vendor/twbs/bootstrap/fonts/glyphicons-halflings-regular.ttf', 'public/fonts/glyphicons-halflings-regular.ttf')
+    .copy('vendor/twbs/bootstrap/fonts/glyphicons-halflings-regular.woff', 'public/fonts/glyphicons-halflings-regular.woff')
+    .copy('vendor/twbs/bootstrap/fonts/glyphicons-halflings-regular.woff2', 'public/fonts/glyphicons-halflings-regular.woff2');
 });
