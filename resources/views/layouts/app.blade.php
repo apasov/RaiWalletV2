af4ce7e3cf208e546be44452feaf506a47f960c4
Jame <jaimehgb@gmail.com>
2018-02-01 17:12:54 +0100

    Some color adjustements to match nano brand

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -48,7 +48,7 @@
                         <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                             <span class="sr-only">Toggle navigation</span> Menu <i class="fa fa-bars"></i>
                         </button>
-                        <a class="navbar-brand page-scroll" href="#page-top"><img src="img/nanologo2.png" class="nav-logo" /></a>
+                        <a class="navbar-brand page-scroll" href="#page-top"><img src="img/toplogo2.png" class="nav-logo" /></a>
                     </div>
 
                     <!-- Collect the nav links, forms, and other content for toggling -->
